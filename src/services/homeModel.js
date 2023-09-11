493afd8d078d2dc6a55cb717db1024028d9f95e2
DanKT15 <105506442+DanKT15@users.noreply.github.com>
2023-09-11 12:44:06 +0700

    11/9/2023

--- src/services/homeModel.js
+++
@@ -1,8 +0,0 @@
-import con from "./../config/connectDB"
-
-const getall = async () => {
-    const [rows, fields] = await con.execute('SELECT * FROM `danhmuc`')
-    return rows
-} 
-
-export default {getall}
